e6b139c14a83aeb2bc3822c9ef03e54c461499e9
Sachiterasu <37966000+SachihoWolfy@users.noreply.github.com>
2023-09-29 11:03:45 -0700

    Nametags?

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using TMPro;
 
 public class PlayerController : MonoBehaviourPun
 {
@@ -12,6 +13,7 @@ public class PlayerController : MonoBehaviourPun
     [Header("Components")]
     public Rigidbody rig;
     [Header("Arms and Headlight")]
+    public TextMeshPro playername;
     public GameObject arms;
     public GameObject head;
     [Header("Look Sensitivity")]
@@ -143,6 +145,7 @@ public class PlayerController : MonoBehaviourPun
         {
             GameUI.instance.Initialize(this);
             head.SetActive(false);
+            playername.text = player.NickName;
         }
     }
 
